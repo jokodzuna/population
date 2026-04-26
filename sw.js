ded949d4490046385989485641e7fe102d69e9f7
Povilas Laurisonis <jokodzuna@gmail.com>
2026-04-26 22:11:35 +0100

    SW_suicide_wipe_all_caches_no_fetch_handler

--- sw.js
+++ sw.js
@@ -1,34 +1,11 @@
-const CACHE_NAME = 'popquiz-v9';
-const ASSETS = [
-  './',
-  './index.html',
-  './game.js',
-  './style.css',
-  './population-data.json',
-  './facts.json',
-  './icon.svg',
-  './icon-maskable.svg',
-  './manifest.json'
-];
-
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
+self.addEventListener('install', () => {
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (e) => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request))
+    caches.keys()
+      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
   );
 });
